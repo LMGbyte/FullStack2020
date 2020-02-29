e00819087b959b0f0e31740d7ac73f227a1832b8
M.Gecas <m.gecas@hotmail.com>
2020-03-01 01:56:59 +0200

    Homework 2020 02 27

---
+++ HomeWork/hw_2_27/scripts/jscript.js
@@ -0,0 +1,34 @@
+let num = 3;
+alert(num);
+
+let a = 10, b = 2;
+document.clear();
+document.write(`${a} + ${b} = ${a+b}<br>`);
+document.write(`${a} - ${b} = ${a-b}<br>`);
+document.write(`${a} * ${b} = ${a*b}<br>`);
+document.write(`${a} / ${b} = ${a/b}<br>`);
+document.write(`<hr>`);
+
+let c = 15, d = 2;
+let result = c + d;
+document.write(result);
+
+a = 17, b = 10;
+c = a - b;
+d = 7;
+result = c + d;
+console.log(result);
+document.write(`<hr>`);
+
+let str = 'Hello World!';
+document.write(`${str} <br>`);
+let str1 = 'Hello', str2 = 'World!';
+document.write(`${str1} ${str2}<br>`);
+document.write(`<hr>`);
+
+let name = 'Mindaugas';
+document.write(`Hello, ${name}!<br>`);
+let age = 69;
+document.write(`I'm ${age} old!<br>`);
+name = prompt(`What's Your name?`);
+document.write(`Your name ${name}<br>`);
