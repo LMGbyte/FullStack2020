1ed1060347e2429e7e5daeac0251afa90bb6910e
Mindaugas Gecas <m.gecas@hotmail.com>
2020-03-18 16:28:46 +0100

    comm

--- HomeWork/hw_3_16/scripts/script.js
+++ HomeWork/hw_3_16/scripts/script.js
@@ -1,8 +1,47 @@
-document.getElementById('start-btn').addEventListener('click', function () {
-    let x = document.getElementById('m-obj').style.left;
-    console.log(x);
-    let mx = 250;
-    document.getElementById('m-obj').style.left = mx +'px';
-    console.log(document.getElementById('m-obj').style.left);
+let moving = false;
+let forward = true;
+let mobj = document.getElementById('m-obj');
+let curr_pos = mobj.style.left;
+let curr_pos_num = parseInt(curr_pos.substring(0,curr_pos.length-2));
+
+let stepx = parseInt(document.getElementById('speed-in').value);
+
+let max_x = document.getElementsByClassName('main')[0].clientWidth;
+
+document.getElementById('start-btn').addEventListener('click', function(){
+    if (!moving){move(stepx)};
+});
+document.getElementById('stop-btn').addEventListener('click', function(){
+    moving = false;
 });
 
+function move(stepx) {
+    moving = true;
+    var pos = curr_pos_num;
+    var id = setInterval(frame, 1);
+    function frame() {
+      stepx = parseInt(document.getElementById('speed-in').value);
+      if (!moving) {
+          curr_pos_num = pos;
+          clearInterval(id);
+      } else {
+        if (forward) {
+            pos+=stepx;
+        } else {
+            pos-=stepx;
+        }; 
+        mobj.style.left = pos + 'px'; 
+        max_x = document.getElementsByClassName('main')[0].clientWidth;
+        if (pos >= max_x-50 && forward){
+            forward = false;
+        };
+        if (pos <= 0 && !forward){
+            forward = true;
+        }
+      }
+    }
+
+}
+// Usefull functions....
+
+// setTimeout(function (){console.log('time ok')}, 6000);
