589e186a2bd0f0f9c16dc322f53fc0b8f7715e7d
Mindaugas Gečas <m.gecas@hotmail.com>
2020-04-25 22:48:43 +0300

    db

---
+++ HomeWork/hw_4_23 SQL/index.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Homework 04 23 (Data base)</title>
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous">
+</head>
+<body>
+
+<div class="card m-3 text-center">
+    <h5 class="card-header">Darbuotojai</h5>
+    <div class="card-body">
+
+        <form>
+            <div class="row">
+                <div class="col-sm-4"></div>
+            <div class="form-group col-sm-4">
+                <label for="salaryInput">Įveskite atlyginimą...</label>
+                <input type="number" class="form-control" id="salaryInput" aria-describedby="emailHelp">
+            </div>
+                <div class="col-sm-4"></div>
+            </div>
+            <button type="submit" class="btn btn-primary">Pateikti sąrašą</button>
+        </form>
+
+    </div>
+</div>
+
+</body>
+</html>
